ce1a45e26b22f9969febb063bd8182ec08a938b9
Yoann Teruel <yoann.teruel@gmail.com>
2024-08-26 12:22:50 +0200

    feat(BoardDetails): add back button

--- src/app/boards/[id]/layout.tsx
+++ src/app/boards/[id]/layout.tsx
@@ -1,6 +1,8 @@
 import { EditDuotone } from "@/components/_ui/Icons";
 import { Container } from "@/components/_ui/Container";
 import React from "react";
+import Link from "next/link";
+import { MaterialSymbol } from "react-material-symbol-icons";
 
 export default function Layout({
   children,
@@ -12,7 +14,11 @@ export default function Layout({
   return (
     <Container>
       <div className="flex flex-col gap-10 w-full">
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-5">
+          <Link href="/boards" className="group flex items-center gap-3">
+            <MaterialSymbol icon="arrow_back" size={24} />{" "}
+            <span className="group-hover:underline">Back to boards</span>
+          </Link>
           <div className="flex gap-4 items-center">
             <h2 className="text-4xl">My Task board</h2>
             <EditDuotone width={24} />
