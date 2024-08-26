c49b7c2feffb1cb8d092b210600c274032cdc308
Yoann Teruel <yoann.teruel@gmail.com>
2024-08-26 10:43:10 +0200

    feat(Boards): improve design

--- src/app/boards/page.tsx
+++ src/app/boards/page.tsx
@@ -1,6 +1,15 @@
-import { Anchor } from "@/components/_ui/Anchor";
 import { getBoardsByUserId } from "@/data-access/board";
 import { assertAuthenticated } from "@/lib/session";
+import { Container } from "@/components/_ui/Container";
+import Link from "next/link";
+import { MaterialSymbol } from "react-material-symbol-icons";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/_ui/DropdownMenu";
+import { Button } from "@/components/_ui/Button";
 
 export default async function Page() {
   const user = await assertAuthenticated();
@@ -8,13 +17,49 @@ export default async function Page() {
   const boards = await getBoardsByUserId(user.id);
 
   return (
-    <div>
-      <h1>List of boards</h1>
-      {boards.map((board) => (
-        <Anchor key={board.id} href={`/boards/${board.id}`}>
-          {board.title}
-        </Anchor>
-      ))}
-    </div>
+    <Container>
+      <div className="flex flex-col gap-10 items-center w-full">
+        <h2 className="text-4xl">My boards</h2>
+        {boards.map((board) => (
+          <div
+            key={board.id}
+            className="min-w-full flex justify-between items-center"
+          >
+            <Link
+              href={`/boards/${board.id}`}
+              className="text-xl hover:underline"
+            >
+              {board.title}
+            </Link>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <div className="h-10 w-10 inline-flex items-center justify-center rounded-full transition-colors hover:bg-gray-300">
+                  <MaterialSymbol icon={"more_vert"} size={24} />
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="space-y-2">
+                <DropdownMenuItem asChild>
+                  <Link
+                    href="#"
+                    className="flex gap-2 items-center cursor-pointer"
+                  >
+                    Edit
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <Link
+                    href="#"
+                    className="flex gap-2 items-center cursor-pointer text-wontdo-500"
+                  >
+                    Delete
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        ))}
+        <Button className="self-start">Add new board</Button>
+      </div>
+    </Container>
   );
 }
